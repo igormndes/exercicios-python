44cf1e19707814dfc3261cd17aec6eaeb3013b45
Igor Mendes <92588244+igormndes@users.noreply.github.com>
2022-02-22 15:57:23 -0300

    Update 2-1.py

--- 2-1/2-1.py
+++ 2-1/2-1.py
@@ -7,7 +7,6 @@ Segunda edição - Junho/2014 - ISBN 978-85-7522-408-3
 Terceira Edição - Janeiro/2019 - ISBN 978-85-7522-718-3
 Site: https://python.nilo.pro.br/
 Arquivo: exercicios3\capitulo 02\exercicio-02-01.py
-
 Converta as seguintes expressões matemáticas para que possam ser calculadas usando o interpretador Python.
 10 + 20 × 30
 4² ÷ 30
@@ -22,10 +21,10 @@ print (f"Gabarito 01: {gabarito_1} \n")
 
 # para potencializações utilizar **
 calculo_2 = 4 ** 2 / 30
-print (f"Cálculo 02 :{calculo_2}")
+print (f"Cálculo 02 :{calculo_2:.2f}")
 
 gabarito_2 = 4 ** 2 / 30 
-print (f"Gabarito 02: {gabarito_2} \n")
+print (f"Gabarito 02: {gabarito_2:.2f} \n")
 
 calculo_3 = (9 ** 4 + 2) * 6 - 1 
 print (f"Cálculo 03 :{calculo_3}")
