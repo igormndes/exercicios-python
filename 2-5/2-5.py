2a6a548105a65db80748ca5f573faf6badae8878
Igor Mendes <92588244+igormndes@users.noreply.github.com>
2022-02-22 17:39:12 -0300

    Create 2-5.py

---
+++ 2-5/2-5.py
@@ -0,0 +1,29 @@
+"""
+Parte do livro Introdução à Programação com Python
+Autor: Nilo Ney Coutinho Menezes
+Editora Novatec (c) 2010-2020
+Primeira edição - Novembro/2010 - ISBN 978-85-7522-250-8
+Segunda edição - Junho/2014 - ISBN 978-85-7522-408-3
+Terceira Edição - Janeiro/2019 - ISBN 978-85-7522-718-3
+Site: https://python.nilo.pro.br/
+Arquivo: Arquivo: exercicios3\capitulo 02\exercicio-02-05.py
+
+
+Escreva um programa que calcule a soma de três variáveis e imprima o resultado na tela.
+
+"""
+
+a = 375
+b = 121
+c = 504
+
+soma = a + b + c
+
+# 1 
+print (a + b + c)
+
+# 2
+print (f"{a + b + c}")
+
+# 3
+print (soma)
