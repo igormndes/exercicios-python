61207f020a7dfb2d730b0332252e390cce218bf9
Igor Mendes <92588244+igormndes@users.noreply.github.com>
2022-02-22 15:52:06 -0300

    Create 2-1.py

---
+++ 2-1/2-1.py
@@ -0,0 +1,33 @@
+"""
+Parte do livro Introdução à Programação com Python
+Autor: Nilo Ney Coutinho Menezes
+Editora Novatec (c) 2010-2020
+Primeira edição - Novembro/2010 - ISBN 978-85-7522-250-8
+Segunda edição - Junho/2014 - ISBN 978-85-7522-408-3
+Terceira Edição - Janeiro/2019 - ISBN 978-85-7522-718-3
+Site: https://python.nilo.pro.br/
+Arquivo: exercicios3\capitulo 02\exercicio-02-01.py
+
+Converta as seguintes expressões matemáticas para que possam ser calculadas usando o interpretador Python.
+10 + 20 × 30
+42 ÷ 30
+(94 + 2) × 6 - 1
+"""
+
+calculo_1 = 10 + 20 * 30
+print (f"Cálculo 01 :{calculo_1}")
+
+gabarito_1 = 10 + 20 * 30
+print (f"Gabarito 01: {gabarito_1} \n")
+
+calculo_2 = 42 / 30
+print (f"Cálculo 02 :{calculo_2}")
+
+gabarito_2 = 4 ** 2 / 30 
+print (f"Gabarito 02: {gabarito_2} \n")
+
+calculo_3 = (94 + 2) * 6 - 1 
+print (f"Cálculo 03 :{calculo_3}")
+
+gabarito_3 = ( 9 ** 4 + 2) * 6  - 1 
+print (f"Gabarito 03: {gabarito_3} \n")
