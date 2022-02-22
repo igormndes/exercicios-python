d53830d7021ebbedf93c96c747d079ed04071a5d
Igor Mendes <92588244+igormndes@users.noreply.github.com>
2022-02-22 17:21:45 -0300

    Create 2-4.py

---
+++ 2-4/2-4.py
@@ -0,0 +1,19 @@
+"""
+Parte do livro Introdução à Programação com Python
+Autor: Nilo Ney Coutinho Menezes
+Editora Novatec (c) 2010-2020
+Primeira edição - Novembro/2010 - ISBN 978-85-7522-250-8
+Segunda edição - Junho/2014 - ISBN 978-85-7522-408-3
+Terceira Edição - Janeiro/2019 - ISBN 978-85-7522-718-3
+Site: https://python.nilo.pro.br/
+Arquivo: Arquivo: exercicios3\capitulo 02\exercicio-02-04.py
+
+Escreva um programa que exiba o resultado de 2a × 3b, em que a vale 3 e b vale 5.
+
+"""
+
+a = 3
+b = 5
+funcao = (2 * a) * ( 3 * b) 
+
+print(f"O resultado da função é : {funcao}")
