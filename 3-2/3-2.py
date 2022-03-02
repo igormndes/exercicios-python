4960d93495e523b47d306fb86543080f8dcb6419
Igor Mendes <92588244+igormndes@users.noreply.github.com>
2022-03-02 09:09:16 -0300

    Create 3-2.py

---
+++ 3-2/3-2.py
@@ -0,0 +1,54 @@
+"""
+Parte do livro Introdução à Programação com Python
+Autor: Nilo Ney Coutinho Menezes
+Editora Novatec (c) 2010-2020
+Primeira edição - Novembro/2010 - ISBN 978-85-7522-250-8
+Segunda edição - Junho/2014 - ISBN 978-85-7522-408-3
+Terceira Edição - Janeiro/2019 - ISBN 978-85-7522-718-3
+Site: https://python.nilo.pro.br/
+Arquivo: Arquivo: exercicios3\capitulo 03\exercicio-03-02.py
+
+Complete a tabela a seguir, respondendo True ou False. Considere a = 4, b = 10, c = 5.0, d = 1 e f = 5.
+
+Expressões:
+a == c
+a < b
+d > b
+c != f
+a == b
+c < d
+b > a
+c >= f
+f >= c 
+c <= c
+c <= f
+
+"""
+
+# Definindo variáveis:
+
+a = 4
+b = 10
+c = 5.0
+d = 1
+f = 5
+
+# Definindo as expressões em uma lista:
+
+expressoes = [
+    a == c,
+    a < b,
+    d > b,
+    c != f,
+    a == b,
+    c < d,
+    b > a,
+    c >= f,
+    f >= c,
+    c <= c,
+    c <= f 
+    ]
+
+# Resultado das expressões:
+
+print (f" Resultado das expressões: {expressoes[0:]}")
